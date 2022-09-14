cebb1033c54938116a8e19b1975bd3d8f0ce2e58
Ratshitimba Murendwa <murendwaratshitimba@gmail.com>
2022-09-14 13:35:40 +0200

    Generate an ssh key using three algorithms task complete

--- git_manager.py
+++ git_manager.py
@@ -1,18 +1,56 @@
+from curses.ascii import isdigit
 from os import system
 import emoji
 
+"""
+Welcome Message.
+"""
 def welcome():
 
-    print("""********************************************
+    print("""**********************************************************
 Welcome to gitlab/github ssh and gpg keys manager program
 
 by Murendwa Ratshitimba
 **********************************************************
     """)
 
+
+"""
+Generate the ssh key using just an algorithm
+"""
 def ssh_key_command(algorithm):
 
-    system("ssh-key-gen -t " + algorithm)
+    system("ssh-keygen -t " + algorithm)
+
+
+"""
+Generate an an ssh key using algorithm and the bit size
+"""
+def ssh_key_command_bits(algorithm, bits):
+
+    system("ssh-keygen -t " + algorithm + " -b " + bits)
+
+
+def valid_bits():
+
+    while True:
+
+        bit_size = input("Specify the bits size [minimum is 1024 bits]: ").strip()
+
+        if bit_size.isdigit() :
+
+            if int(bit_size) >= 1024:
+
+
+                return bit_size
+            
+            else:
+
+                print("\nprovide bits is less than 1024!!\n")
+        
+        else:
+
+            print("Please specify valid digits")
 
 
 def generate_ssh_key():
@@ -21,41 +59,45 @@ def generate_ssh_key():
     Generate ssh key 
     """
 
+    algorithims = ["ED25519", "RSA", "ECDSA"]
+    
+
     while True:
 
         algorithim = input("""Choose Algorithm
 
-        1. ED25519
-        2. RSA
-        3. ED25519_SK
-        4. ECDSA
+        1. ED25519 [recommended]
+        2. RSA [At least 2048 bits size is recommended]
+        3. ECDSA
 
         NB: Only choose a number from the list above
 
-        """).strip().lower()
+        Option: """).strip().lower()
+
+        print()
 
 
         if algorithim == "1":
 
-            ssh_key_command("ed25519")
+            ssh_key_command(algorithims[0])
 
         elif algorithim == "2":
 
-            pass
+            bit_size = valid_bits()
+            ssh_key_command_bits(algorithims[1], bit_size)
+
+            
 
         elif algorithim == "3":
 
-            pass
-
-        elif algorithim == "4":
-            pass
+            ssh_key_command(algorithims[2])
 
         else:
 
-            print("""******
-            Invalid Input..
-            ****************
-            \n\n""")
+            print("""
+****************
+Invalid Option
+****************\n""")
 
 
 def main():
@@ -63,6 +105,7 @@ def main():
     # welcome the user
     welcome()
     generate_ssh_key()
+
 
 if __name__ == "__main__":
 
